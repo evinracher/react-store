af582272875841aca5d595f01249168432ee91b2
evinracher <eevinley@gmail.com>
2022-04-03 12:23:01 -0500

    Fixing layout class

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer__title">PlatziConf Store</p>
+      <p className="Footer__copy">All rights reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Header__title">PlatziConf Store</h1>
+      <div className="Header__checkout">Checkout</div>
+    </div>
+  );
+};
+
+export default Header;

779c8790540fb3e8b10c62e96e56931916fdbca7
evinracher <eevinley@gmail.com>
2022-04-03 12:59:05 -0500

    25: Maquetación y estilos de la información del usuario

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Checkout from "../containers/Checkout";
 import Home from "../containers/Home";
-import Info from "../containers/Info";
+import Information from "../containers/Information";
 import NotFound from "../containers/NotFound";
 import Payment from "../containers/Payment";
 import Success from "../containers/Success";
@@ -14,7 +14,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/checkout" element={<Checkout />} />
-          <Route exact path="/checkout/info" element={<Info />} />
+          <Route exact path="/checkout/info" element={<Information />} />
           <Route exact path="/checkout/payment" element={<Payment />} />
           <Route exact path="/checkout/success" element={<Success />} />
           <Route path="*" element={<NotFound />} />
